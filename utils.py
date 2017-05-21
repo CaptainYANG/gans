b5028dd56e2d7db83f2daba7bdda3caaf7c49cd4
yuyin_yang <yangyuyin@yangyuyindembp.localdomain>
2017-05-22 00:16:22 +0200

    read about cycle gans

---
+++ utils.py
@@ -0,0 +1,53 @@
+from __future__ import division
+import math
+import json
+import random
+import pprint
+import scipy.misc
+import numpy as np
+from time import gmtime, strftime
+from six.moves import xrange
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+pp = pprint.PrettyPrinter()
+
+get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
+
+def show_all_variables():
+  model_vars = tf.trainable_variables()
+  slim.model_analyzer.analyze_vars(model_vars, print_info=True)
+
+def get_image(image_path, input_height, input_width,
+              resize_height=64, resize_width=64,
+              is_crop=True, is_grayscale=False):
+  image = imread(image_path, is_grayscale)
+  return transform(image, input_height, input_width,
+                   resize_height, resize_width, is_crop)
+
+def imread(path, is_grayscale = False):
+  if (is_grayscale):
+    return scipy.misc.imread(path, flatten = True).astype(np.float)
+  else:
+    return scipy.misc.imread(path).astype(np.float)
+
+def transform(image, input_height, input_width, 
+              resize_height=64, resize_width=64, is_crop=True):
+  if is_crop:
+    cropped_image = center_crop(
+      image, input_height, input_width, 
+      resize_height, resize_width)
+  else:
+    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
+  return np.array(cropped_image)/127.5 - 1.
+
+def center_crop(x, crop_h, crop_w,
+                resize_h=64, resize_w=64):
+  if crop_w is None:
+    crop_w = crop_h
+  h, w = x.shape[:2]
+  j = int(round((h - crop_h)/2.))
+  i = int(round((w - crop_w)/2.))
+  return scipy.misc.imresize(
+      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
